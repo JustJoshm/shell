252e7c38c5ebe47df1ca11070dd2527c7fc6bc76
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-14 07:57:24 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -6,15 +6,6 @@
 ## edit your Config Vars in heroku/app/settings and 
 ## restart the bot by turning the worker off and back on
 
-echo "Creating config.json..."
-cd /home/shell-bot
-cat > config.json << EOF
-{
-    "authToken": "7423823798:AAGKiCEcRPXZGEx1G1FtDa4bzm9IqOZ4loU",
-    "owner": 1348352154
-}
-EOF
-echo "Done."
-
+cd node
 echo "Starting Bot..."
 node server
